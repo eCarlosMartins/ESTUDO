fe5e4b2b01b187250e91da913cebb013957d6228
Carlos <carlosmaze@gmail.com>
2024-08-07 21:59:41 -0300

    07082024

---
+++ AUTOMAÇÃO/js/listagem-notas.js
@@ -0,0 +1,16 @@
+var botao = document.querySelector("button")
+var tabela = document.querySelector(".bodytabela")
+
+
+var notas = []
+
+/*
+    {
+            "itens": [],
+            "numero_nota": 0,
+            "cliente": "",
+            "cnpj/cpf": "",
+            "data_criacao": "2024/08/07Z12:00:00"
+        }
+*/
+
