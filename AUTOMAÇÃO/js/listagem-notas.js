a524b119253691ed29c39c6b1c5f1d2522868e51
Carlos <carlosmaze@gmail.com>
2024-08-20 21:58:06 -0300

    20082024

--- AUTOMAÇÃO/js/listagem-notas.js
+++ AUTOMAÇÃO/js/listagem-notas.js
@@ -1,16 +1,95 @@
-var botao = document.querySelector("button")
+var botao = document.querySelector(".maisum")
 var tabela = document.querySelector(".bodytabela")
+var modal = document.querySelector(".modal")
+console.log(modal.attributes["data-open"])
 
+var notas = [
+ {
+    numero: 1,
+    cliente: "Péricles",
+    item: "chave de fenda",
+    data: new Date()
+},
+{
+    numero: 2,
+    cliente: "Péricles",
+    item: "chave de fenda",
+    data: new Date()
+},
+{
+    numero: 3,
+    cliente: "Péricles",
+    item: "chave de fenda",
+    data: new Date()
+},
+{
+    numero: 4,
+    cliente: "Péricles",
+    item: "chave de fenda",
+    data: new Date()
+}
+]
 
-var notas = []
+function refresh(){
+    tabela.innerHTML = ""
+    notas.forEach(element => {
+        tabela.innerHTML += `
+        <tr>
+                    <td>${element.numero}</td>
+                    <td>${element.cliente}</td>
+                    <td>${element.item}</td>             
+                    <td>${element.data.toLocaleDateString("pt-BR") }</td>             
+                </tr>`
+    });
+}  
+refresh()
 
-/*
-    {
-            "itens": [],
-            "numero_nota": 0,
-            "cliente": "",
-            "cnpj/cpf": "",
-            "data_criacao": "2024/08/07Z12:00:00"
+function adicionar(valores){
+        notas.push({
+            numero: Math.floor(Math.random()*1000),
+            cliente: valores.cliente,
+            item: valores.item,
+            data: valores.data
+        })
+        refresh()
+}
+
+function subimetido (evento) {
+    evento.preventDefault()
+    
+    //var numero = document.querySelector("#numero").value;
+    var cliente = document.querySelector("#cliente").value;
+    var item = document.querySelector("#item").value;
+    var data = document.querySelector("#data").value;
+
+    if(cliente===""||item===""|| data===""){
+        window.alert("reveja os parametros e veja novamente")
         }
-*/
+    else{
+        adicionar({
+            cliente,
+            item,
+            data: new Date(data)
+        })
+        abreefecha()
+    }
+}
 
+
+function abreefecha(){
+    document.querySelector("form").addEventListener('submit', subimetido)
+    var aberto = modal.attributes["data-open"].value
+    if(aberto==="true"){
+        modal.style.display="none"
+        modal.attributes["data-open"].value="false"   
+     }
+     else{
+        modal.style.display="flex"
+        modal.attributes["data-open"].value="true";
+     }
+}
+
+
+botao.addEventListener(
+    "click", abreefecha
+)
